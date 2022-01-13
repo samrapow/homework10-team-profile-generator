b5ca023ce2b7d126095e7725bc6547ffa87f5e09
Sam Rapowitz <samrapow@gmail.com>
2022-01-12 21:32:32 -0500

    Added employee.js file

--- employees/employee.js
+++ employees/employee.js
@@ -1,3 +1,26 @@
 class Employee {
-    constructor(name, employeeID, emailAddress)
-}
+    constructor(name, employeeID, emailAddress) {
+        this.name = name;
+        this.employeeID = employeeID;
+        this.emailAddress = emailAddress;
+    };
+
+    getName() {
+        return(this.name);
+    }
+
+    getEmployeeID() {
+        return(this.id);
+    }
+
+    getEmailAddress() {
+        return(this.emailAddress);
+    }
+
+    getJob() {
+        return "Employee";
+    }
+
+};
+
+module.exports = Employee;
