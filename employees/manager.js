596326753185b5a1ff9a76d3d8ef5a39b8a21149
Sam Rapowitz <samrapow@gmail.com>
2022-01-12 21:37:36 -0500

    Added manager.js

---
+++ employees/manager.js
@@ -0,0 +1,19 @@
+const { MODULEDECLARATION_TYPES } = require("@babel/types");
+const Employee = require("../employees/employee.js")
+
+class Manager extends Employee {
+    constructor(name, employeeID, emailAddress, officeNumber) {
+        super(name, employeeID, emailAddress);
+        this.officeNumber = officeNumber;
+    }
+
+    getOfficeNumber() {
+        return(this.officeNumber);
+    }
+
+    getJob() {
+        return 'Manager';
+    }
+};
+
+module.exports = Manager;
